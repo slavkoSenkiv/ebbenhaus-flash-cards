522c9487725e01a9e915240b6c61486b014c0f78
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2022-01-23 10:11:40 +0200

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,14 +1,14 @@
-import openpyxl
-wb_name = 'words collection.xlsx'
-bank_deck_name = 'bank_deck'
-working_deck_name = 'working_deck'
-words_collection_wb = openpyxl.open(wb_name)
+class GrandParent:
+    def __init__(self):
+        self.lst = []
 
 
-def clear_deck(self):
-    for sheet in words_collection_wb.sheetnames:
-        if sheet.startswith(bank_deck_name):
-            del words_collection_wb[sheet]
-    words_collection_wb.create_sheet(self.deck_name)
-    words_collection_wb.save(wb_name)
-    self.deck = {}
+class Parent(GrandParent):
+    def __init__(self):
+        super(Parent, self).__init__()
+
+
+class Child(Parent):
+    def __init__(self):
+        super(Child, self).__init__()
+        
